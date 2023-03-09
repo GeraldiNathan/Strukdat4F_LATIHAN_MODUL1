97089a15184e5f3f7f03aae0ca6ec1e0ca757d6a
GeraldiNathan <124975715+GeraldiNathan@users.noreply.github.com>
2023-03-09 18:36:38 +0800

    Add files via upload

---
+++ Strukdat_Latihan1_Modul1/src/Kota.java
@@ -0,0 +1,18 @@
+    public class Kota<E> {
+        private E element;
+
+        public Kota (E kota){
+            element = kota;
+        }
+
+        public E getElement(){
+            return element;
+        }
+
+        public static void main(String[] args) {
+            Kota<Integer> jumlahkota = new Kota<Integer>(9);
+            Kota<String> namakota = new Kota<String>("Malang");
+            System.out.println("Jumlah Kota Di Jatim : " + jumlahkota.getElement() + " Kota");
+            System.out.println("Salah satu kota di jatim :" + namakota.getElement());
+        }
+    }
